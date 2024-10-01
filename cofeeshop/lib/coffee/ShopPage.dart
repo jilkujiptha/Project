fd40fd8770b809be9cc10a329229b3ee12b84840
jilkujiptha <mpjiptha@gmail.com>
2024-10-01 18:00:37 +0530

    coffeeshop

--- cofeeshop/lib/coffee/ShopPage.dart
+++ cofeeshop/lib/coffee/ShopPage.dart
@@ -8,24 +8,49 @@ class ShoPage extends StatefulWidget {
 }
 
 class _ShoPageState extends State<ShoPage> {
+  // List ls=[
+  //   {
+
+  //   }
+  // ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        backgroundColor: Colors.brown[900],
+        backgroundColor: Colors.grey[900],
         title: Center(
           child: Text(
             "MOKA",
             style: TextStyle(
-                fontFamily: "Schyler",
-                color: const Color.fromARGB(255, 255, 170, 59)),
+                fontFamily: "edu",
+                color: const Color.fromARGB(255, 255, 170, 59),fontSize: 30),
           ),
         ),
       ),
-      body: Container(child: ListView.builder(itemBuilder: (context, index) {
-        return Card(
-        );
-      })),
+      body:
+           Column(
+             children: [
+              Text("Its a Great Day for Coffee",style: TextStyle(color: Colors.white,fontSize: 25),),
+               Expanded(
+                 child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    childAspectRatio: 3/4,
+                    crossAxisCount: 2
+                    ),
+                    padding: EdgeInsets.all(10), 
+                  itemBuilder: (context,index){
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color:  const Color.fromARGB(255, 245, 240, 240)
+                      ),
+                    );
+                 }),
+               ),
+             ],
+           ),
     );
   }
 }
