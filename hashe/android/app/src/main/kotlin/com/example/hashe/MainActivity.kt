b84f49042d3ff82f5d740f749fc8f181e275ff40
jilkujiptha <mpjiptha@gmail.com>
2024-10-04 17:55:33 +0530

    hashe

---
+++ hashe/android/app/src/main/kotlin/com/example/hashe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hashe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ hashe/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hashe/Hashe/homePage.dart';
+void main(){
+  runApp(MaterialApp(home: HomePage(),));
+}
